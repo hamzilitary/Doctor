80cc03c4207181dbf89278d1f21d6c61dd000ddd
Guest User <Guest@Epicodus-4C.local>
2018-03-23 13:13:57 -0700

    add doctor.js and main.js and API key

---
+++ src/doctor.js
@@ -0,0 +1,20 @@
+export class Doctor{
+  constructor(){}
+  getDoctor() {
+    let request = new XMLHttpRequest();
+
+    let url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=b31152f926f686ef9570a34b03a33afe';
+
+    return new Promise(function(resolve, reject){
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  };
+}
--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Doctor} from './doctor.js';
+
+$(document).ready(function(){
+  $('#doctorFind').click(function() {
+    event.preventDefault();
+    let name = $('#doctorName').val();
+    $('#doctorName').val("");
+    let doctor = new Doctor();
+    let promise = doctor.getDoctor(name);
+
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      // let doctorArray = [];
+      // body.practices.name
+      $('.showProvider').text(`These are the doctors in your area: ${body.data.profile}.`);
+    },
+     function(error){
+      $('.errors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+});

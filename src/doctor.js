b38b36b6fba226e220b24a2b1833e2b2fe1aa9f0
Guest User <Guest@Epicodus-7A.local>
2018-03-31 16:32:20 -0700

    re visited promise method got first name , last name and accepting patients

--- src/doctor.js
+++ src/doctor.js
@@ -1,54 +1,23 @@
-import $ from 'jquery';
 
 export class Doctor{
   constructor(){}
-  getDoctor(doctorName, searchDoctors) {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctorIssue}&location=or-portland&skip=0&limit=10&user_key=b31152f926f686ef9570a34b03a33afe`)
-    .then(response => {
-      if ( response.data.length === 0) {
-        $('#showSymptom').text('No Result Found')
-      } else {
-        console.log(response);
-        searchDoctors(response);
-      }
-    })
+  getDoctor(query) {
+    let request = new XMLHttpRequest();
 
+    let url =
 
+    `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=10&user_key=b31152f926f686ef9570a34b03a33afe`;
 
-  }
-
-  // getCondition(doctorCondition, searchDoctors){
-  //
-
-  }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let request = new XMLHttpRequest();
-
-// let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=b31152f926f686ef9570a34b03a33afe';
-//
-// return new Promise(function(resolve, reject){
-//   request.onload = function() {
-//     if (this.status === 200) {
-//       resolve(request.response);
-//     } else {
-//       reject(Error(request.statusText));
-//     }
-//   };
-//   request.open("GET", url, true);
-//   request.send();
-// });
-// };
+    return new Promise(function(resolve, reject){
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  };
+}
--- src/main.js
+++ src/main.js
@@ -4,79 +4,90 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Doctor} from './doctor.js';
 
-let findDoctors = response => {
-  for (let i=0; i < response.data.length; i++) {
-    let name = response.data[i].profile.first_name + ' ' + resonse.data[i].profile.last_name;
-    let acceptsPatients;
-    if (response.data[i].practices[0].accepts_new_patients == true) {
-      acceptsPatients = "Accepting";
-    } else {
-      acceptsPatients = "Not Accepting";
-    }
-    $('#showDoctor').append('<li>' + 'Name:' + ' ' + name + ' ' + 'Accepting Patients:' + ' ' + acceptsPatients);
-
-  }
-};
-
-$(document).ready(() => {
-  let DoctorFinder = new Doctor();
-  $('#doctorSearch').click(function(){
-    event.preventDefault();
-    $('#showDoctor').empty();
-    let doctorName = $('#doctor').val();
-    $('#showDoctor').text();
-    DoctorFinder.getDoctor(doctorName, findDoctors);
-  })
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function(){
-//   $('#doctorFind').click(function() {
+// let findDoctors = response => {
+//   for (let i=0; i < response.data.length; i++) {
+//     let name = response.data[i].profile.first_name + ' ' + resonse.data[i].profile.last_name;
+//     let acceptsPatients;
+//     if (response.data[i].practices[0].accepts_new_patients == true) {
+//       acceptsPatients = "Accepting";
+//     } else {
+//       acceptsPatients = "Not Accepting";
+//     }
+//     $('#showDoctor').append('<li>' + 'Name:' + ' ' + name + ' ' + 'Accepting Patients:' + ' ' + acceptsPatients);
+//
+//   }
+// };
+//
+// $(document).ready(() => {
+//   let DoctorFinder = new Doctor();
+//   $('#doctorSearch').click(function(){
 //     event.preventDefault();
-//     let name = $('#doctorName').val();
-//     $('#doctorName').val("");
-//     let doctor = new Doctor();
-//     let promise = doctor.getDoctor(name);
-//
-//     promise.then(function(response){
-//       let body = JSON.parse(response);
-//       let doctorArray = [""];
-//       Object.keys(body.data[0].profile).forEach(function(element){
-//          doctorArray.push(element);
-//
-//
-//          // JUST REALIZED I SHOULD USE A FOR LOOP 4:42PM
+//     $('#showDoctor')
+//   })
+// })
 //
 //
 //
-//         //  Object.keys(thisDoctor.bio).forEach(function(element){
-//         //   doctorArray.push(thisDoctor.bio);
-//         //  });
-//
-//         console.log(body.data[0].profile);
-//
-//               // doctorArray.push(thisDoctor);
-//       });
-//       doctorArray.forEach(function(element){
-//         $(".showProvider").append("<p>" + element + "</p>");
-//       });
-//       // body.practices.name
-//       // $('.showProvider').text(`These are the doctors in your area: ${body.data.first_name}.`);
-//     },
-//      function(error){
-//       $('.errors').text(`There was an error processing your request: ${error.message}`);
-//     });
-//   });
-//
-// });
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $('#doctorFind').click(function() {
+    event.preventDefault();
+    let name = $('#doctorName').val();
+    $('#doctorName').val("");
+    let doctor = new Doctor();
+    let promise = doctor.getDoctor(name);
+
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      let doctorArray = [""];
+      // for (let i = 0; i < response.data.length; i++)
+
+      body.data.forEach(function(doc){
+        doctorArray.push(doc.profile.first_name + ' ' + doc.profile.last_name );
+      });
+      body.data.forEach(function(doc){
+        if (doc.practices.accepts_new_patients == true){
+          doctorArray.push("yes Accepting");
+        } else {
+          doctorArray.push("yes Accepting");
+        }
+      })
+
+
+      // Object.keys(body.data[0].profile).forEach(function(element){
+      //    doctorArray.push(body.data[0].profile[element]);
+
+
+         // JUST REALIZED I SHOULD USE A FOR LOOP 4:42PM
+
+
+
+        //  Object.keys(thisDoctor.bio).forEach(function(element){
+        //   doctorArray.push(thisDoctor.bio);
+        //  });
+
+        console.log(body.data[0].profile);
+
+              // doctorArray.push(thisDoctor);
+      // });
+      doctorArray.forEach(function(element){
+        $(".showProvider").append("<p>" + element + "</p>");
+      });
+      // body.practices.name
+      // $('.showProvider').text(`These are the doctors in your area: ${body.data.first_name}.`);
+    },
+     function(error){
+      $('.errors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+});

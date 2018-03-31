6c91c0ad2183de054e773e75dcac91b4308a4427
Guest User <Guest@Epicodus-7A.local>
2018-03-31 14:47:58 -0700

    to access APIs and seperate logic with out using a promise

--- src/doctor.js
+++ src/doctor.js
@@ -1,20 +1,52 @@
+import $ from 'jquery';
+
 export class Doctor{
   constructor(){}
-  getDoctor() {
-    let request = new XMLHttpRequest();
+  getDoctor(doctorName, searchDoctors) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctorIssue}&location=or-portland&skip=0&limit=10&user_key=b31152f926f686ef9570a34b03a33afe`)
+    .then(response => {
+      if ( response.data.length === 0) {
+        $('#showSymptom').text('No Result Found')
+      } else {
+        searchDoctors(response);
+      }
+    })
 
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=b31152f926f686ef9570a34b03a33afe';
 
-    return new Promise(function(resolve, reject){
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-  };
+
+  }
+
+  getCondition(){
+
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let request = new XMLHttpRequest();
+
+// let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=b31152f926f686ef9570a34b03a33afe';
+//
+// return new Promise(function(resolve, reject){
+//   request.onload = function() {
+//     if (this.status === 200) {
+//       resolve(request.response);
+//     } else {
+//       reject(Error(request.statusText));
+//     }
+//   };
+//   request.open("GET", url, true);
+//   request.send();
+// });
+// };
